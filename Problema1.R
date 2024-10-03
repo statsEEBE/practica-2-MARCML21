d0452cc729641fd007505d0db8c4152f8ef4fb93
marc.martinez.luis <marc.martinez.luis@auleseebe.upc.edu>
2024-10-03 11:28:57 +0200

    prob 1 pt 2

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,12 @@
 #Codigo para problema 1
 
+#trobeyu eñ coeficient de determinacio o de bonat d'ajust
+
+Rsq  <- 0.192 #(multiple r-squared)
+R <- sqrt(Rsq)
+
+#quina variacio esperem del valor del mercat per augment de una unitat dels actius(pendiente)
+m <-0.0658
+b <- 11.789 #(intercept)
+x <- 647.1
+m*x+b
